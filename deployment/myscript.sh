c44577f6f8023c9b4b6b2e0718df62cb02c48a5a
JAZ1086 <joshuaazumah1086@gmail.com>
2022-10-24 02:32:40 +0100

    changes to ci files

---
+++ deployment/myscript.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Appliquer les variables d'environnement et les secrets
+# kubectl apply -f aws-secret.yaml
+# kubectl apply -f env-secret.yaml
+# kubectl apply -f env-configmap.yaml
+# 
+# kubectl apply -f backend-feed-deployment.yaml
+# kubectl apply -f backend-feed-service.yaml
+# 
+# kubectl apply -f backend-user-deployment.yaml
+# kubectl apply -f backend-user-service.yaml
+# 
+# 
+# kubectl apply -f frontend-deployment.yaml
+# kubectl apply -f frontend-service.yaml
+# 
+# 
+# kubectl apply -f reverseproxy-deployment.yaml
+# kubectl apply -f reverseproxy-service.yaml
+# 
+
+# echo "-------------------Removing Old Pods--------------------"
+# kubectl delete deploy backend-user
+# kubectl delete deploy backend-feed
+# kubectl delete deploy frontend
+# kubectl delete deploy reverseproxy
+
+echo "-------------------Creating new Pods--------------------"
+kubectl apply -f backend-feed-deployment.yaml
+kubectl apply -f backend-user-deployment.yaml
+kubectl apply -f frontend-deployment.yaml
+kubectl apply -f reverseproxy-deployment.yaml
+
+kubectl apply -f backend-feed-service.yaml
+kubectl apply -f backend-user-service.yaml
+kubectl apply -f frontend-service.yaml
+kubectl apply -f reverseproxy-service.yaml
+
