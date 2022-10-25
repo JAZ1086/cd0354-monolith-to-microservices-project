89a08a03729927ea29a367af33beb5946211c5b8
JAZ1086 <joshuaazumah1086@gmail.com>
2022-10-25 21:06:35 +0100

    making changes  to myscript file to delete the pods

--- deployment/myscript.sh
+++ deployment/myscript.sh
@@ -17,17 +17,17 @@ kubectl delete deploy reverseproxy
 # kubectl delete deploy frontend
 # kubectl delete deploy reverseproxy
 
-echo "-------------------Creating new Pods--------------------"
-kubectl apply -f backend-feed-deployment.yaml
-kubectl apply -f backend-user-deployment.yaml
-kubectl apply -f frontend-deployment.yaml
-kubectl apply -f reverseproxy-deployment.yaml
-
-kubectl apply -f backend-feed-service.yaml
-kubectl apply -f backend-user-service.yaml
-kubectl apply -f frontend-service.yaml
-kubectl apply -f reverseproxy-service.yaml
-
+# echo "-------------------Creating new Pods--------------------"
+# kubectl apply -f backend-feed-deployment.yaml
+# kubectl apply -f backend-user-deployment.yaml
+# kubectl apply -f frontend-deployment.yaml
+# kubectl apply -f reverseproxy-deployment.yaml
+# 
+# kubectl apply -f backend-feed-service.yaml
+# kubectl apply -f backend-user-service.yaml
+# kubectl apply -f frontend-service.yaml
+# kubectl apply -f reverseproxy-service.yaml
+# 
 # kubectl delete hpa reverseproxy
 # kubectl autoscale deployment backend-feed --cpu-percent=50 --min=1 --max=10
 # kubectl autoscale deployment backend-user --cpu-percent=50 --min=1 --max=10
