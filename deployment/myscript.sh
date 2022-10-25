9bf9d81efa25d606d3242ab47afe367c6afd97c2
JAZ1086 <joshuaazumah1086@gmail.com>
2022-10-25 15:36:34 +0100

    add hpa screenshots

--- deployment/myscript.sh
+++ deployment/myscript.sh
@@ -6,27 +6,33 @@
 # kubectl apply -f env-configmap.yaml
 # 
 # 
-# echo "-------------------Removing Old Pods--------------------"
+echo "-------------------Removing Old Pods--------------------"
+kubectl delete deploy backend-user
+kubectl delete deploy backend-feed
+kubectl delete deploy frontend
+kubectl delete deploy reverseproxy
+# 
 # kubectl delete deploy backend-user
 # kubectl delete deploy backend-feed
 # kubectl delete deploy frontend
 # kubectl delete deploy reverseproxy
 
-# echo "-------------------Creating new Pods--------------------"
-# kubectl apply -f backend-feed-deployment.yaml
-# kubectl apply -f backend-user-deployment.yaml
-# kubectl apply -f frontend-deployment.yaml
-# kubectl apply -f reverseproxy-deployment.yaml
-# 
-# kubectl apply -f backend-feed-service.yaml
-# kubectl apply -f backend-user-service.yaml
-# kubectl apply -f frontend-service.yaml
-# kubectl apply -f reverseproxy-service.yaml
+echo "-------------------Creating new Pods--------------------"
+kubectl apply -f backend-feed-deployment.yaml
+kubectl apply -f backend-user-deployment.yaml
+kubectl apply -f frontend-deployment.yaml
+kubectl apply -f reverseproxy-deployment.yaml
 
-kubectl autoscale deployment backend-feed --cpu-percent=50 --min=1 --max=10
-kubectl autoscale deployment backend-user --cpu-percent=50 --min=1 --max=10
-kubectl autoscale deployment frontend --cpu-percent=50 --min=1 --max=10
-kubectl autoscale deployment reverseproxy --cpu-percent=50 --min=1 --max=10
+kubectl apply -f backend-feed-service.yaml
+kubectl apply -f backend-user-service.yaml
+kubectl apply -f frontend-service.yaml
+kubectl apply -f reverseproxy-service.yaml
+
+# kubectl delete hpa reverseproxy
+# kubectl autoscale deployment backend-feed --cpu-percent=50 --min=1 --max=10
+# kubectl autoscale deployment backend-user --cpu-percent=50 --min=1 --max=10
+# kubectl autoscale deployment frontend --cpu-percent=50 --min=1 --max=10
+# kubectl autoscale deployment reverseproxy --cpu-percent=50 --min=1 --max=10
 
 
 
